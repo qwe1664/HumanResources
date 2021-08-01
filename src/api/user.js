c5b42292a52af946cdc3a11cbab8f3f3ef36ea45
xiaolun <2242969756@qq.com>
2021-08-01 09:37:56 +0800

    封装单独的登录接口

--- src/api/user.js
+++ src/api/user.js
@@ -1,6 +1,13 @@
 import request from '@/utils/request'
 
+// 登录接口的封装
 export function login(data) {
+  // 返回一个promise 对象
+  return request({
+    url: '/sys/login',
+    method: 'post',
+    data
+  })
 }
 
 export function getInfo(token) {
