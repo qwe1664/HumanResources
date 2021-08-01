2a6caebf5aaadf50f2afc57bfbad4c8abdbdebc0
xiaolun <2242969756@qq.com>
2021-08-01 22:12:38 +0800

    在main.js中完成自定义指令全局注册

--- src/main.js
+++ src/main.js
@@ -11,6 +11,8 @@ import '@/styles/index.scss' // global css
 import App from './App'
 import store from './store'
 import router from './router'
+
+import * as directives from '@/directives'
 import '@/icons' // icon
 import '@/permission' // permission control
 
@@ -18,6 +20,10 @@ import '@/permission' // permission control
 Vue.use(ElementUI, { locale })
 // 如果想要中文版 element-ui，按如下方式声明
 // Vue.use(ElementUI)
+
+Object.keys(directives).forEach(key => {
+  Vue.directive(key, directives[key])  // 注册自定义指令
+})
 
 Vue.config.productionTip = false
 
