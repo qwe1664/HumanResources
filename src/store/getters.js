8e79e1e97eb8edadc0dfe76ae2661c03c052c847
xiaolun <2242969756@qq.com>
2021-08-06 22:06:21 +0800

    读取公司信息数据

--- src/store/getters.js
+++ src/store/getters.js
@@ -4,6 +4,7 @@ const getters = {
   token: state => state.user.token, // 建立token的快捷访问
   name: state => state.user.userInfo.username, // 建立对于用户名称的映射
   userId: state => state.user.userInfo.userId, // 建立对于用户名的快捷访问
-  staffPhoto: state => state.user.userInfo.staffPhoto // 建立头像的快捷访问
+  staffPhoto: state => state.user.userInfo.staffPhoto, // 建立头像的快捷访问
+  companyId: state => state.user.userInfo.companyId // 建立公司id的快捷访问
 }
 export default getters
--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -11,10 +11,11 @@
               <el-button size="small" icon="el-icon-plus" type="primary">新增角色</el-button>
             </el-row>
             <!-- 表格 -->
-            <el-table border>
-              <el-table-column label="序号" width="120px"></el-table-column>
-              <el-table-column label="名称" width="240px"></el-table-column>
-              <el-table-column label="描述"></el-table-column>
+            <!-- 给表格绑定数据 -->
+            <el-table border :data="list">
+              <el-table-column align="center" type="index" label="序号" width="120px"></el-table-column>
+              <el-table-column prop="name" label="名称" width="240px"></el-table-column>
+              <el-table-column prop="description" label="描述"></el-table-column>
               <el-table-column label="操作">
                 <el-button size="small" type="success">分配权限</el-button>
                 <el-button size="small" type="primary">编辑</el-button>
@@ -22,8 +23,15 @@
               </el-table-column>
             </el-table>
             <!-- 放置分页组件 -->
+            <!-- 放置分页组件 -->
             <el-row type="flex" justify="center" align="middle" style="height:60px">
-              <el-pagination layout="prev,pager,next"></el-pagination>
+              <el-pagination
+                :current-page="page.page"
+                :page-size="page.pagesize"
+                :total="page.total"
+                layout="prev, pager, next"
+                @current-change="changePage"
+              />
             </el-row>
           </el-tab-pane>
           <!-- 右侧内容 -->
@@ -38,16 +46,25 @@
             <!-- 在表单中显示公司的基本信息，不允许修改 -->
             <el-form label-width="120px" style="margin-top:50px">
               <el-form-item label="公司名称">
-                <el-input disabled style="width:400px" />
+                <el-input v-model="formDate.name" disabled style="width:400px" />
               </el-form-item>
               <el-form-item label="公司地址">
-                <el-input disabled style="width:400px" />
+                <el-input v-model="formDate.companyAddress" disabled style="width:400px" />
+              </el-form-item>
+              <el-form-item label="电话">
+                <el-input v-model="formDate.companyPhone" disabled style="width:400px" />
               </el-form-item>
               <el-form-item label="邮箱">
-                <el-input disabled style="width:400px" />
+                <el-input v-model="formDate.mailbox" disabled style="width:400px" />
               </el-form-item>
               <el-form-item label="备注">
-                <el-input type="textarea" :rows="3" disabled style="width:400px" />
+                <el-input
+                  v-model="formDate.remarks"
+                  type="textarea"
+                  :rows="3"
+                  disabled
+                  style="width:400px"
+                />
               </el-form-item>
             </el-form>
           </el-tab-pane>
@@ -58,7 +75,45 @@
 </template>
 
 <script>
-export default {};
+import { getRoleList, getCompanyInfo } from "@/api/setting";
+import { mapGetters } from "vuex";
+export default {
+  data() {
+    return {
+      list: [], // 用于存放请求数据返回的数组
+      page: {
+        // 放置页码及相关数据
+        page: 1,
+        pagesize: 10,
+        total: 0 // 记录总数
+      },
+      formDate: {
+        // 公司信息
+      }
+    };
+  },
+  computed: {
+    ...mapGetters(["companyId"])
+  },
+  created() {
+    this.getRoleList(); // 获取角色列表
+    this.getCompanyInfo();
+  },
+  methods: {
+    async getRoleList() {
+      const { total, rows } = await getRoleList();
+      this.page.total = total;
+      this.list = rows;
+    },
+    async getCompanyInfo() {
+      this.formDate = await getCompanyInfo(this.companyId); // 赋值给fromDate
+    },
+    changePage(newPage) {
+      this.page.page = newPage;
+      this.getRoleList();
+    }
+  }
+};
 </script>
 
 <style>
